9066942e304e1eda54d0744d269e3934bafe7c1f
Ouchi-hiroshi-76 <h-ouchi@createtech.org>
2025-10-30 13:33:28 +0900

    10/30
    link/sitemap

--- page-120-sitemap.php
+++ page-120-sitemap.php
@@ -1,15 +1,15 @@
     <?php
-      /*
+    /*
       Template Name: サイトマップ
       */
-      ?>
+    ?>
 
-      <!--  header -->
-      <?php get_header('120'); ?>
-      <!--  header -->
+    <!--  header -->
+    <?php get_header('120'); ?>
+    <!--  header -->
 
-      <main class="c-main">
-      <div class="c-page-header lazyload" data-bg="<?php echo esc_url(get_template_directory_uri()); ?>/img/header/page-header7.jpg">
+    <main class="c-main">
+      <div class="c-page-header lazyload">
         <div class="c-column c-page-header__inner">
           <h1 class="c-page-header__title">Sitemap
             <span>サイトマップ</span>
@@ -18,59 +18,83 @@
       </div>
 
       <section class="_mtl">
-        <div class="c-column-s">
-          <h3 class="sitemap-home"><a href="<?php echo esc_url(home_url('')); ?>/">ホーム</a></h3>
-          <div class="page-links">
-            <div class="page-links-half">
-              <!-- <section>
-                <h3 class="title-sitemap"><a href="<?php echo esc_url(home_url('')); ?>/club/">（要確認）当倶楽部について</a></h3>
-              </section> -->
-              <section>
-                <h3 class="title-sitemap"><a href="<?php echo esc_url(home_url('')); ?>/news/">ニュース</a></h3>
-              </section>
+        <div class="c-column">
 
-              <section>
-                <h3 class="title-sitemap"><a href="<?php echo esc_url(home_url('')); ?>/course/">コース案内</a></h3>
-              </section>
+          <div class="c-box">
+            <div class="page-links">
+              <div class="page-links-half">
+                <section>
+                  <h3 class="title-sitemap"><a href="<?php echo esc_url(home_url('')); ?>">ホーム</a></h3>
+                </section>
+                <section>
+                  <h3 class="title-sitemap"><a href="<?php echo esc_url(home_url('')); ?>">ご予約方法について</a></h3>
+                </section>
 
-              <section>
-                <h3 class="title-sitemap"><a href="<?php echo esc_url(home_url('')); ?>/facility/">施設案内</a></h3>
-              </section>
+                <section>
+                  <h3 class="title-sitemap"><a href="<?php echo esc_url(home_url('')); ?>">レストラン</a></h3>
+                </section>
 
-              <section>
-                <h3 class="title-sitemap">ご利用案内</h3>
-                <ul class="page-link">
-                  <li><a href="<?php echo esc_url(home_url('')); ?>/guide/#gd-member">- プレー料金</a></li>
-                  <li><a href="<?php echo esc_url(home_url('')); ?>/guide/#gd-twilight">- 練習場</a></li>
-                  <li><a href="<?php echo esc_url(home_url('')); ?>/restaurant/">- レストラン</a></li>
-                  <li><a href="<?php echo esc_url(home_url('')); ?>/guide/#gd-rental">- ご予約について</a></li>
-                </ul>
-              </section>
+                <section>
+                  <h3 class="title-sitemap"><a href="<?php echo esc_url(home_url('')); ?>">プライベートルーム</a></h3>
+                </section>
 
-            </div>
+                <section>
+                  <h3 class="title-sitemap"><a href="<?php echo esc_url(home_url('')); ?>">施設案内</a></h3>
+                </section>
 
-            <div class="page-links-half">
+                <section>
+                  <h3 class="title-sitemap"><a href="<?php echo esc_url(home_url('')); ?>">コース紹介</a></h3>
+                </section>
 
-              <section>
-                  <h3 class="title-sitemap"><a href="<?php echo esc_url(home_url('')); ?>/restaurant/">レストラン</a></h3>
-              </section>
+                <section>
+                  <h3 class="title-sitemap"><a href="<?php echo esc_url(home_url('')); ?>">アクセス・近隣ホテル情報</a></h3>
+                </section>
 
-              <section>
-                <h3 class="title-sitemap"><a href="<?php echo esc_url(home_url('')); ?>/access/">アクセス</a></h3>
-              </section>
+                <section>
+                  <h3 class="title-sitemap"><a href="<?php echo esc_url(home_url('')); ?>">ニュース</a></h3>
+                </section>
 
-              <section>
-              <h3 class="title-sitemap"><a href="<?php echo esc_url(home_url('')); ?>/privacypolicy/">プライバシーポリシー</a></h3>
-              </section>
+                <section>
+                  <h3 class="title-sitemap"><a href="<?php echo esc_url(home_url('')); ?>">倶楽部概要</a></h3>
+                </section>
 
-              <!-- <section>
-                <h3 class="title-sitemap"><a href="<?php echo esc_url(home_url('')); ?>/links/">リンク集</a></h3>
-              </section> -->
+              </div>
 
-              <section>
-                <h3 class="title-sitemap"><a href="https://docs.google.com/forms/d/e/1FAIpQLSdGGDErtO1ZTQLWl9yCv5Vm7N-vBBwSTNbAtkpgTDZNk3up1g/viewform" target="_blank">お問い合わせ</a></h3>
-              </section>
+              <div class="page-links-half">
 
+                <section>
+                  <h3 class="title-sitemap"><a href="<?php echo esc_url(home_url('')); ?>">ゴルフ練習場概要</a></h3>
+                </section>
+
+                <section>
+                  <h3 class="title-sitemap"><a href="<?php echo esc_url(home_url('')); ?>">求人情報</a></h3>
+                </section>
+
+                <section>
+                  <h3 class="title-sitemap"><a href="<?php echo esc_url(home_url('')); ?>">会員募集について</a></h3>
+                </section>
+
+                <section>
+                  <h3 class="title-sitemap"><a href="<?php echo esc_url(home_url('')); ?>">ドレスコード</a></h3>
+                </section>
+
+                <section>
+                  <h3 class="title-sitemap"><a href="<?php echo esc_url(home_url('')); ?>">サイトポリシー</a></h3>
+                </section>
+
+                <section>
+                  <h3 class="title-sitemap"><a href="<?php echo esc_url(home_url('')); ?>">プライバシーポリシー</a></h3>
+                </section>
+
+                <section>
+                  <h3 class="title-sitemap"><a href="<?php echo esc_url(home_url('')); ?>">リンク集</a></h3>
+                </section>
+
+                <section>
+                  <h3 class="title-sitemap"><a href="<?php echo esc_url(home_url('')); ?>">お問い合わせフォーム</a></h3>
+                </section>
+
+              </div>
             </div>
           </div>
 
@@ -86,14 +110,12 @@
 
     </main>
 
-        <!--  フッタ読込 -->
-        <?php get_footer('120'); ?>
-        <!--  フッタ読込 -->
+    <!--  フッタ読込 -->
+    <?php get_footer('120'); ?>
+    <!--  フッタ読込 -->
 
-          <?php wp_footer(); ?>
+    <?php wp_footer(); ?>
 
-        </body>
+    </body>
 
-</html>
-
-    
+    </html>
