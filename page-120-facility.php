d31d42899fa733e67b64099e126639a24e6af46b
Ouchi-hiroshi-76 <h-ouchi@createtech.org>
2025-11-13 14:51:16 +0900

    11/13
    施設紹介

--- page-120-facility.php
+++ page-120-facility.php
@@ -31,7 +31,7 @@
       </div>
     </div>
 
-    <ul class="c-scroll _scr01">
+    <ul class="c-scroll">
       <li><a href="#link01">エントランス</a></li>
       <li><a href="#link02">レストラン</a></li>
       <li><a href="#link03">プライベートルーム</a></li>
@@ -41,8 +41,8 @@
       <li><a href="#link07">練習場</a></li>
     </ul>
 
-    <section>
-      <h2 class="c-head6 _scr02" id="link01">エントランス<span>Entrance</span></h2>
+    <section class="_scr01" id="link01">
+      <h2 class="c-head6">エントランス<span>Entrance</span></h2>
       <div class="facility__flex">
         <div class="facility__info">
           <div>
@@ -51,9 +51,11 @@
         </div>
         <span class="facility__flex--img"><img src="<?php echo esc_url(get_template_directory_uri()); ?>/img/facility/img_facility_01.jpg" alt="エントランス" loading="lazy"></span>
       </div>
+    </section>
 
-      <h2 class="c-head6 _scr02" id="link02">レストラン<span>Reataurant</span></h2>
-      <div class="facility__flex _norev">
+    <section class="_scr01" id="link02">
+      <h2 class="c-head6">レストラン<span>Reataurant</span></h2>
+      <div class="facility__flex">
         <div class="facility__info">
           <div>
             <p>フロントの説明テキストが入ります。フロントの説明テキストが入ります。フロントの説明テキストが入ります。</p>
@@ -66,11 +68,11 @@
   </div>
 
   <!-- プライベートルーム -->
-  <section class="bgchen">
+  <section class="_scr01 bgchen" id="link03">
     <span class="deco _01"><span></span></span>
     <span class="deco _02"><span></span></span>
     <div class="c-column">
-      <h2 class="c-head6 _scr02" id="link03">プライベートルーム<span>Privateroom</span></h2>
+      <h2 class="c-head6" id="link03">プライベートルーム<span>Privateroom</span></h2>
       <p>大切なお客様にゆっくりとご利用いただける個室を15室ご用意しております。</p>
       <div class="facility__flex">
         <div class="facility__info">
@@ -147,13 +149,13 @@
             <td>すぎ</td>
             <td colspan="2">12人</td>
           </tr>
-          <tr>
-            <td class="proom__tb--color">もみじ①②</td>
+          <tr class="proom__tb--color">
+            <td>もみじ①②</td>
             <td>4人</td>
-            <td rowspan="2">景観重視の<br class="c-brpc">お部屋です</td>
+            <td rowspan="2">景観重視の<br>お部屋です</td>
           </tr>
-          <tr>
-            <td class="proom__tb--color">もみじ③</td>
+          <tr class="proom__tb--color">
+            <td>もみじ③</td>
             <td>8人</td>
           </tr>
         </table>
@@ -165,99 +167,100 @@
     </div>
   </section>
 
-  <section>
-    <div class="c-column">
-      <div class="fs-div">
-        <div class="facility__flex">
-          <div class="facility__info">
-            <p>クラブハウス玄関を入って右側にフロント。<br>シンプルでやや広めのカウンターからスタッフが笑顔でお迎えいたします。</p>
-          </div>
-          <span class="facility__flex--img"><img src="<?php echo esc_url(get_template_directory_uri()); ?>/img/facility/img_facility_01.jpg" alt="" loading="lazy"></span>
-        </div>
-
-        <div class="facility__flex">
-          <div class="facility__info">
-            <h3 class="c-head3">コンペルーム</h3>
-            <p>レストランと隣接し2室。クラブハウス3階には3室。<br>数人から80人のパーティにも対応できます。<br>パーティ料理も予算に応じてお受けいたします。</p>
-          </div>
-          <span class="facility__flex--img"><img src="<?php echo esc_url(get_template_directory_uri()); ?>/img/facility/img_facility_02.jpg" alt="" loading="lazy"></span>
-        </div>
-
-        <div class="facility__flex">
-          <div class="facility__info">
-            <h3 class="c-head3">ロッカールーム</h3>
-            <p>ナラの木の木目を生かしたイエローゴールド色の落ち着きのある色彩となっております。<br>又、男子・女子とも、それぞれの浴室に隣接していますので大変使いやすいロッカールームです。</p>
-          </div>
-          <span class="facility__flex--img"><img src="<?php echo esc_url(get_template_directory_uri()); ?>/img/facility/img_facility_03.jpg" alt="" loading="lazy"></span>
-        </div>
-
-        <div class="fs-div">
-          <h2 class="c-head1" id="link02">
-            レストラン
-            <span class="c-head1__en">restaurant</span>
-          </h2>
-
-          <div class="facility__flex">
-            <div class="facility__info">
-              <p>レストラン西側窓正面に雄大な富士山を！<br>南側窓からはコースを望みながら、チーフ自慢の料理をご堪能下さい。</p>
-            </div>
-            <span class="facility__flex--img"><img src="<?php echo esc_url(get_template_directory_uri()); ?>/img/facility/img_facility_04.jpg" alt="" loading="lazy"></span>
-          </div>
-
-          <a href="<?php echo esc_url(home_url('')); ?>/restaurant/" class="c-btn">レストラン詳細へ</a>
-        </div>
-
-        <div class="fs-div">
-          <h2 class="c-head1" id="link03">
-            浴場
-            <span class="c-head1__en">bathroom</span>
-          </h2>
-
-          <div class="facility__flex">
-            <div class="facility__info">
-              <h3 class="c-head3">男性浴場</h3>
-              <p>男子浴室の中央に円形の浴槽。浴室の大窓のキャンパスに雄大な富士山を眺めながら一日の疲れを癒してください。<br>脱衣室の床は健康に良く殺菌作用があるといわれている本物の竹タイルを用いております。<br>又、一角にマッサージ機をご用意しておりますので、湯上りのひと時にお寛ぎ下さい。</p>
-            </div>
-            <span class="facility__flex--img"><img src="<?php echo esc_url(get_template_directory_uri()); ?>/img/facility/img_facility_05.jpg" alt="" loading="lazy"></span>
-          </div>
-
-          <div class="facility__flex">
-            <div class="facility__info">
-              <h3 class="c-head3">女性浴場</h3>
-              <p>２Ｆに構える女性浴場は、浴槽ごしに外の景観を友にしながら、癒しを誘い、富士山の恵み「鉱泉の湯けむり」が肌を潤す。<br>鉱泉＝温泉より低温の、鉱物質をふくむ湧水。<br>浴槽ごしの硝子は外部からは見えない特殊硝子です。ご安心下さい。</p>
-            </div>
-            <span class="facility__flex--img"><img src="<?php echo esc_url(get_template_directory_uri()); ?>/img/facility/img_facility_06.jpg" alt="" loading="lazy"></span>
-          </div>
-
-        </div>
-
-        <div class="fs-div">
-          <h2 class="c-head1" id="link04">
-            練習場
-            <span class="c-head1__en">drivingrange</span>
-          </h2>
-
-          <div class="facility__flex">
-            <div class="facility__info">
-              <h3 class="c-head3">ドライビングレンジ「上達空間 遊球の森」</h3>
-              <p class="_mts">ワンコイン（30球）　300円(税込330円) </p>
-              <ul class="c-list">
-                <li><span>※</span>お一人様ワンコインを原則とさせて頂きます。</li>
-                <li><span>※</span>キャディマスター室でコインをお求めください。（オープン時間は正午12時まで）</li>
-                <li><span>※</span>但しプロレッスン・レッスンスクール等でのご利用の場合はこの限りではありません。</li>
-              </ul>
-              <p class="_mts">【お願い】</p>
-              <ul class="c-list">
-                <li><span>1.</span>打席数8打席のため混み合う場合がございます。ご利用の皆様のご協力をお願い申し上げます。</li>
-                <li><span>2.</span>ドライビングレンジの芝生内は大変危険です。芝生内への立ち入りは禁止させて頂きます。</li>
-              </ul>
-            </div>
-            <span class="facility__flex--img"><img src="<?php echo esc_url(get_template_directory_uri()); ?>/img/facility/img_facility_07.jpg" alt="" loading="lazy"></span>
+  <div class="c-column">
+    <section class="_scr01" id="link04">
+      <h2 class="c-head6" id="link03">ロッカールーム<span>Locker&nbsp;room</span></h2>
+      <div class="facility__flex">
+        <div class="facility__info">
+          <div>
+            <p>気品と落ち着きを兼ね備えたロッカールーム。<br class="c-brpc">広々とした動線、美しい木製ロッカー、天窓から差し込む柔らかな自然光に包まれてゆったりとした時間をお過ごしください。<br class="c-brpc">プレー前の高揚感も、ラウンド後の余韻も、この空間が穏やかに包み込みます。</p>
           </div>
         </div>
+        <span class="facility__flex--img"><img src="<?php echo esc_url(get_template_directory_uri()); ?>/img/facility/img_facility_05.jpg" alt="ロッカールーム" loading="lazy"></span>
       </div>
-    </div>
-  </section>
+    </section>
+
+
+    <section class="_scr01" id="link05">
+      <h2 class="c-head6" id="link03">バスルーム<span>Largebathhouse</span></h2>
+      <div class="facility__flex">
+        <div class="facility__info">
+          <div>
+            <h3 class="facility__info--sub">更衣室</h3>
+            <p>ゴルフを満喫したあとは、広々とした屋内浴場で心と体を癒してください。<br class="c-brpc">大きな窓越しに広がる緑豊かな景色が、まるで森の中にいるような非日常のひとときを演出します。<br class="c-brpc">やわらかな光と静けさに包まれながら、贅沢なリラックスタイムをお楽しみいただけます。</p>
+          </div>
+        </div>
+        <span class="facility__flex--img"><img src="<?php echo esc_url(get_template_directory_uri()); ?>/img/facility/img_facility_06.jpg" alt="更衣室" loading="lazy"></span>
+      </div>
+
+      <div class="facility__flex">
+        <div class="facility__info">
+          <div>
+            <h3 class="facility__info--sub">大浴場</h3>
+            <p>ゴルフを満喫したあとは、広々とした屋内浴場で心と体を癒してください。<br class="c-brpc">大きな窓越しに広がる緑豊かな景色が、まるで森の中にいるような非日常のひとときを演出します。<br class="c-brpc">やわらかな光と静けさに包まれながら、贅沢なリラックスタイムをお楽しみいただけます。</p>
+          </div>
+        </div>
+        <span class="facility__flex--img"><img src="<?php echo esc_url(get_template_directory_uri()); ?>/img/facility/img_facility_07.jpg" alt="大浴場" loading="lazy"></span>
+      </div>
+
+      <div class="facility__flex">
+        <div class="facility__info">
+          <div>
+            <h3 class="facility__info--sub">サウナ</h3>
+            <p>上質な天然木を贅沢に使用した、ゆったりとくつろげるサウナをご用意しております。<br class="c-brpc">木の香りとやわらかな温もりに包まれながら、心と身体をゆっくりと整えるひとときをお過ごしください。</p>
+          </div>
+        </div>
+        <span class="facility__flex--img"><img src="<?php echo esc_url(get_template_directory_uri()); ?>/img/facility/img_facility_08.jpg" alt="サウナ" loading="lazy"></span>
+      </div>
+
+      <div class="facility__flex">
+        <div class="facility__info">
+          <div>
+            <h3 class="facility__info--sub">パウダールーム</h3>
+            <p>女性のお客様用の、ゆったりと落ち着いた雰囲気のパウダールームです。大きな鏡と清潔な空間で、リラックスしてお使いいただけます。</p>
+          </div>
+        </div>
+        <span class="facility__flex--img"><img src="<?php echo esc_url(get_template_directory_uri()); ?>/img/facility/img_facility_09.jpg" alt="パウダールーム" loading="lazy"></span>
+      </div>
+    </section>
+
+    <section class="_scr01" id="link06">
+      <h2 class="c-head6   _norev" id="link03">中庭<span>Atrium</span></h2>
+      <div class="facility__flex">
+        <div class="facility__info">
+          <div>
+            <p>館内に足を踏み入れると、ガラス越しに現れるのは、静寂に包まれた美しい中庭。<br class="c-brpc">凛とした竹がすっと伸び、自然と建築が調和したこの庭は、訪れる方の心に静かな感動をもたらします。<br class="c-brpc">プレー前後のひとときに、ぜひその美しさをご鑑賞ください。</p>
+          </div>
+        </div>
+        <span class="facility__flex--img"><img src="<?php echo esc_url(get_template_directory_uri()); ?>/img/facility/img_facility_10.jpg" alt="洋中庭室" loading="lazy"></span>
+      </div>
+    </section>
+
+    <section class="_scr01" id="link07">
+      <h2 class="c-head6" id="link03">ゴルフ練習場<span>Driving&nbsp;range</span></h2>
+      <div class="facility__flex">
+        <div class="facility__info">
+          <div>
+            <h3 class="facility__info--sub">ドライビングレンジ</h3>
+            <p>館内に足を踏み入れると、ガラス越しに現れるのは、静寂に包まれた美しい中庭。<br class="c-brpc">凛とした竹がすっと伸び、自然と建築が調和したこの庭は、訪れる方の心に静かな感動をもたらします。<br class="c-brpc">プレー前後のひとときに、ぜひその美しさをご鑑賞ください。</p>
+          </div>
+        </div>
+        <span class="facility__flex--img"><img src="<?php echo esc_url(get_template_directory_uri()); ?>/img/facility/img_facility_11.jpg" alt="ドライビングレンジ" loading="lazy"></span>
+      </div>
+
+      <div class="facility__flex">
+        <div class="facility__info">
+          <div>
+            <h3 class="facility__info--sub">バンカー</h3>
+            <p>館内に足を踏み入れると、ガラス越しに現れるのは、静寂に包まれた美しい中庭。<br class="c-brpc">凛とした竹がすっと伸び、自然と建築が調和したこの庭は、訪れる方の心に静かな感動をもたらします。<br class="c-brpc">プレー前後のひとときに、ぜひその美しさをご鑑賞ください。</p>
+          </div>
+        </div>
+        <span class="facility__flex--img"><img src="<?php echo esc_url(get_template_directory_uri()); ?>/img/facility/img_facility_12.jpg" alt="バンカー" loading="lazy"></span>
+      </div>
+      <a href="<?php echo esc_url(home_url('')); ?>/d-range/" class="c-btn">ゴルフ練習場概要へ</a>
+    </section>
+  </div>
+
 
   <div class="c-column">
     <ul class="c-brd">
