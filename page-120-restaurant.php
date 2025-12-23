9e0bf7df96e5c09fcc4d40ed6e44e10bbfc8c81c
M-Hatano <m-hatano@createtech.org>
2025-12-23 09:52:35 +0900

    9/23①

--- page-120-restaurant.php
+++ page-120-restaurant.php
@@ -57,7 +57,7 @@
                                 お食事の際、大切なお客様にゆっくりとご利用いただける個室を15室ご用意しております。<br>
                                 お部屋の詳細については施設紹介ページをご覧ください。
                             </p>
-                            <a href="<?php echo esc_url(get_template_directory_uri()); ?>/img/restaurant/private-room.pdf" target="_blank" class="c-btn r-btn">プライベートルーム詳細</a>
+                            <a href="<?php echo esc_url(get_template_directory_uri()); ?>/facility/#link03" target="_blank" class="c-btn r-btn">プライベートルーム詳細</a>
                         </div>
                     </div>
                 </div>
