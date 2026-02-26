f93b19b2e6123ceae927c0d92cfa4c289c7554a7
M-Hatano <m-hatano@createtech.org>
2026-02-26 15:08:27 +0900

    2/26
    会員募集要項掲載

--- include-120-reservation-start.php
+++ include-120-reservation-start.php
@@ -3,5 +3,5 @@
     <p>ご予約のスタートはお電話でお受けしております。</p>
     <a href="<?php echo esc_url(home_url('')); ?>/reservation/">ご予約方法についてはこちら</a>
   </div>
-  <ul class="c-list"><li><span>※</span>ご予約には、平日は会員のご紹介、<br class="c-brsp">土日祝日は、会員の同伴が必要となります</li></ul>
+  <ul class="c-list"><li><span>※</span>ご予約には、平日は会員のご紹介、土日祝日は、会員の同伴が必要となります。</li></ul>
 </div>
